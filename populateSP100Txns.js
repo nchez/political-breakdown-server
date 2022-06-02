d94a8d7a817d7301d50cbfdd8fd8ee374fe895d4
Nathan Chesmar <nathan.chesmar@gmail.com>
2022-06-02 13:22:11 -0700

    finish populateSP500txns.js, populate DB with top 500 stocks and their respective txns

--- populateSP100Txns.js
+++ populateSP100Txns.js
@@ -1,4 +1,7 @@
+require('dotenv').config()
 const fs = require('fs')
+const db = require('./models')
+const axios = require('axios')
 const top500 = fs.readFileSync('top500.json')
 const topStocks = JSON.parse(top500)
 
@@ -16,5 +19,48 @@ const createStockArr = () => {
 
 const stocks = createStockArr()
 
-console.log(stocks)
-console.log(stocks.length)
+const config = {
+  headers: {
+    accept: 'application/json',
+    'X-CSRFToken':
+      'TyTJwjuEC7VV7mOqZ622haRaaUr0x0Ng4nrwSRFKQs7vdoBcJlK9qjAS69ghzhFu',
+    Authorization: 'Token ' + process.env.QUIV_API_KEY,
+  },
+}
+
+const populateTxns = async (arr) => {
+  //   for (let j = 0; j < stocks.length; j++) {
+  let count = 1
+  for (let j = 0; j < stocks.length; j++) {
+    const stockId = await db.Stock.create({
+      name: stocks[j].name,
+      symbol: stocks[j].symbol.toLowerCase(),
+      sector: stocks[j].sector,
+    })
+    const stockSymbol = stocks[j].symbol.toLowerCase()
+    const url = `https://api.quiverquant.com/beta/historical/congresstrading/${stockSymbol}`
+    const stockDb = await db.Stock.findById(stockId)
+    const response = await axios.get(url, config)
+    const transactions = response.data
+    for (let i = 0; i < transactions.length; i++) {
+      const newTxn = await db.Transaction.create({
+        stock: stockDb._id,
+        symbol: transactions[i].Ticker.toLowerCase(),
+        reportDate: new Date(transactions[i].ReportDate),
+        transactionDate: new Date(transactions[i].TransactionDate),
+        representative: transactions[i].Representative,
+        amount: transactions[i].Amount,
+        transaction: transactions[i].Transaction,
+        house: transactions[i].House,
+        range: transactions[i].Range,
+      })
+      stockDb.transactions.push(newTxn._id)
+    }
+    stockDb.save()
+    console.log(`stock ${count} updated`)
+    count++
+  }
+  console.log('done populating')
+}
+
+populateTxns()
